c30066cae7d800acb32f6764cc978ccc80c830ac
Harshid <harshidkoladara3624@gmail.com>
2020-02-20 12:17:33 +0530

    commit

--- app/src/main/java/com/hacks/complainmanagement/signup_fragment.java
+++ app/src/main/java/com/hacks/complainmanagement/signup_fragment.java
@@ -8,6 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +20,10 @@ import android.view.ViewGroup;
  */
 public class signup_fragment extends Fragment {
 
+    TextView signupText;
+    EditText firstName, lastName, contact, email, password;
+    RadioGroup sex;
+    Button signup;
 
     public signup_fragment() {
         // Required empty public constructor
@@ -25,7 +34,21 @@ public class signup_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_fragment, container, false);
+        View v = inflater.inflate(R.layout.fragment_signup_fragment, container, false);
+
+        signupText = v.findViewById(R.id.signup_signuptext);
+        firstName = v.findViewById(R.id.signup_firstname);
+        lastName = v.findViewById(R.id.signup_lastname);
+        contact = v.findViewById(R.id.signup_contact);
+        email = v.findViewById(R.id.signup_email);
+        password = v.findViewById(R.id.signup_password);
+        sex = v.findViewById(R.id.signup_sex);
+        signup = v.findViewById(R.id.signup_button);
+
+
+        return v;
     }
+
+    //TODO:take data make it validate and then add into server
 
 }
